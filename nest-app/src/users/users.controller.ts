e2946efb1405d088da5dd091e36f5d18e266c605
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2024-02-08 00:42:28 +0530

    added NotFoundException handlers in controllers and services

--- nest-app/src/users/users.controller.ts
+++ nest-app/src/users/users.controller.ts
@@ -7,8 +7,10 @@ import {
   Patch,
   Query,
   Delete,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreateUserDto } from './DTOs/create-user.dto';
+import { UpdateUserDto } from './DTOs/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -21,13 +23,23 @@ export class UsersController {
   }
 
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.usersService.findOne(parseInt(id));
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      return new NotFoundException('User not found.');
+    }
+
+    return user;
   }
 
   @Get()
   findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email);
+  }
+
+  @Patch(':id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 
   @Delete('/:id')
